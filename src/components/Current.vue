b0da2c19f96505bfe5b03851938b3f41ebf08963
Nathan Petrangelo <nathan@petrangelo.org>
2023-10-13 22:57:39 -0600

    Tasks as bulleted list

--- src/components/Current.vue
+++ src/components/Current.vue
@@ -1,5 +1,25 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const tasks = ref([{ message: 'Foo' }, { message: 'Bar' }]);
+</script>
+
 <template>
     <div class="col">
         <h1>Current Tasks</h1>
+        <div class="tasks">
+            <li v-for="task in tasks">
+                {{ task.message }}
+            </li>
+        </div>
     </div>
 </template>
+
+<style>
+.tasks {
+    border-radius: 10px;
+    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+    text-align: left;
+    padding-left: 2vh;
+    height: 100%;
+}
+</style>
