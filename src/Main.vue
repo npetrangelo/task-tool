b315bd0d017566dc73048140c68f0d3814ac39fa
Nathan Petrangelo <nathan@petrangelo.org>
2023-10-13 19:51:15 -0600

    Initial commit, some work past starter template

---
+++ src/Main.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Projects</h1>
+                <p>stuff</p>
+                <p>stuff</p>
+                <p>stuff</p>
+                <p>stuff</p>
+                <p>stuff</p>
+            </div>
+            <div class="col">
+                <h1>Current Tasks</h1>
+            </div>
+            <div class="col">
+                <h1>Recently Completed</h1>
+                <h1>Starred Completes</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.logo.vite:hover {
+  filter: drop-shadow(0 0 2em #747bff);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #249b73);
+}
+
+div {
+    border-radius: 8px;
+    border: 1px solid transparent;
+    /* border-color: #249b73; */
+    border-radius: 8px;
+    padding: 0.6em 1.2em;
+    font-size: 0.95em;
+    font-weight: 500;
+    font-family: Arial, Helvetica, sans-serif;
+    color: #0f0f0f;
+    background-color: #ffffff;
+    transition: border-color 0.25s;
+    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+}
+</style>
