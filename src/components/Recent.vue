a3a812e3dfe5e257fb9206560ac5385a77250e7a
Nathan Petrangelo <nathan@petrangelo.org>
2023-10-13 23:07:21 -0600

    All components are dynamic lists

--- src/components/Recent.vue
+++ src/components/Recent.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const tasks = ref([{ message: 'Foo' }, { message: 'Bar' }]);
+</script>
+
 <template>
-    <h1>Recently Completed</h1>
+    <div class="col">
+        <h1>Recently Completed</h1>
+        <div class="col tasks">
+            <li v-for="task in tasks">
+                {{ task.message }}
+            </li>
+        </div>
+    </div>
 </template>
