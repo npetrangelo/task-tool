392b1a6b88dd1eeae7bdf9fd64a222f5695b539f
Nathan Petrangelo <nathan@petrangelo.org>
2023-10-15 16:58:38 -0600

    Visible border

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ import Starred from './components/Starred.vue';
 div {
     border-radius: 5px;
     border: 1px solid transparent;
-    /* border-color: #249b73; */
+    border-color: #249b73;
     padding: 0em 0.5em;
     font-size: 0.95em;
     font-weight: 500;
